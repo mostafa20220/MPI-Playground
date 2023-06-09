402d52bd43fc9dc0a4ac7bcb53b3c4249f3b31da
Mostafa <mostafaasd2018@gmail.com>
2023-06-09 18:33:08 +0300

    Trapezoidal Example (need to fix)
    - The calculations is not correct

--- mpi.c
+++ mpi.c
@@ -3,32 +3,83 @@
 
 const int MAX_STRING = 100;
 
-int main(){
+void getInput(int myRank, int commSize, double *globalStart, double *globalEnd, int *n)
+{
 
-  int comm_size, my_rank;
-  char msg [MAX_STRING];
+  if (myRank == 0)
+  {
+    printf("Enter start, end, and n :\n");
+    fflush(stdout);
+    scanf("%lf %lf %d",globalStart, globalEnd, n);
+
+    for (int dest = 1; dest < commSize; dest++)
+    {
+      MPI_Send(globalStart, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+      MPI_Send(globalEnd, 1, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
+      MPI_Send(n, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+    }
+  }
+  else
+  {
+    MPI_Recv(globalStart, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(globalEnd, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+}
+
+double f(double x)
+{
+  return x*x;
+}
+
+double trap(double localStart, double localEnd ,double h, int n )
+{
+  int i;
+  double estimate = ( f(localStart) + f(localEnd) ) / 2.0;  
+  for (i = 1; i < n; i++)
+  {
+    estimate+= f(localStart + i * h); 
+  }
+  estimate*= h;
+  
+  return estimate;
+}
+
+
+int main()
+{
+
+  int commSize, myRank;
+  // char msg[MAX_STRING];
+
+  double globalStart, globalEnd, globalInteg, localStart, localEnd, localInteg , h;
+  int n, localN;
 
   MPI_Init(NULL, NULL);
-  MPI_Comm_size(MPI_COMM_WORLD,&comm_size);
-  MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
-  if (my_rank != 0)
-  {
-    sprintf(msg,"This is process: %d of %d\n",my_rank,comm_size);
-    MPI_Send(msg,MAX_STRING,MPI_CHAR,0,0,MPI_COMM_WORLD);
-  }
-  else{
+  getInput(myRank, commSize, &globalStart, &globalEnd, &n);
+  h = globalEnd - globalStart / n;
+  localN = n / commSize;
+  localStart = globalStart + myRank * h;
+  localEnd   = localStart  + localN * h;
 
-    printf("This is process: %d of %d\n",my_rank,comm_size);
+  localInteg = trap(localStart,localEnd,h,localN);
 
-    for (int src = 1; src < comm_size; src++)
+  if(myRank!=0){
+    MPI_Send(&localInteg,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+  }else{
+    globalInteg=localInteg;
+    for (size_t i = 1; i < commSize; i++)
     {
-      MPI_Recv(msg,MAX_STRING,MPI_CHAR,src,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-      printf("%s",msg);
+      MPI_Recv(&localInteg,1,MPI_DOUBLE,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      globalInteg+=localInteg;
     }
+    printf("The global Integ= %lf\n",globalInteg);
     
   }
-  
+
   MPI_Finalize();
 
   return 0;
